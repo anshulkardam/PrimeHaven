b840e640d299d84748a5ee48479ed2704b8c370d
anshulkardam <anshulkardamsr@gmail.com>
2025-03-02 19:04:52 +0530

    homepage and profilepage created

---
+++ components/Filters.tsx
@@ -0,0 +1,46 @@
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { useLocalSearchParams, router } from "expo-router";
+import { categories } from "@/constants/data";
+
+const Filters = () => {
+    const params = useLocalSearchParams<{ filter?: string }>();
+    const [selectedCategory, setSelectedCategory] = useState(params.filter || "All");
+
+    const handleCategory = (category: string) => {
+        if (selectedCategory === category) {
+            setSelectedCategory("All");
+            router.setParams({ filter: "All" });
+            return;
+        }
+
+        setSelectedCategory(category);
+        router.setParams({ filter: category });
+    };
+    return (
+        <ScrollView horizontal className="mt-3 mb-2" showsHorizontalScrollIndicator={false}>
+            {categories.map((item, index) => (
+                <TouchableOpacity
+                    onPress={() => handleCategory(item.category)}
+                    className={`flex flex-col items-start mr-4 px-4 py-2 rounded-full ${
+                        selectedCategory === item.category
+                            ? "bg-primary-300"
+                            : "bg-primary-100 border border-primary-200"
+                    }`}
+                >
+                    <Text
+                        className={`text-sm ${
+                            selectedCategory === item.category
+                                ? "text-white font-rubik-bold mt-0.5"
+                                : "text-primary-300 font-rubik"
+                        }`}
+                    >
+                        {item.title}
+                    </Text>
+                </TouchableOpacity>
+            ))}
+        </ScrollView>
+    );
+};
+
+export default Filters;
